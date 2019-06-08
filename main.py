f36de6deeb0c3e7896fe6525aa9afd5e7d492041
Terrasse <3305049949@qq.com>
2019-06-08 23:11:58 +0800

    Oirgin

---
+++ main.py
@@ -0,0 +1,35 @@
+#!usr/bin/python3
+#coding=utf-8
+
+# Split a sentense by JieBa:
+#   https://github.com/fxsjy/jieba
+
+import jieba as jb
+import random as rdm
+
+
+file=open("materials.txt",'r')
+data=file.read().split('\n')
+file.close()
+cnt=int()
+scr=int()
+
+
+if __name__=='__main__':
+    while True:
+        ln=rdm.sample(data,1)[0]
+        ln=jb.lcut(ln)
+        blk=''
+        while len(blk)<2:
+            blk=rdm.sample(ln,1)[0]
+        ln[ln.index(blk)]='？'*len(blk)
+        ans=input(''.join(ln)+"\n-->")
+        if ans=='exit':
+            print("You win %d of %d" % (scr,cnt))
+            exit()
+        if ans==blk:
+            print("RIGHT")
+            scr+=1
+        else:
+            print("WRONG  the answer is",blk)
+        cnt+=1
